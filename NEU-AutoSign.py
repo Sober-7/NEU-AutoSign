d66319bbdac37d53f8453ef259583a4b26f847f9
Chardo-v <36465628+Chardo-v@users.noreply.github.com>
2020-03-12 12:21:56 +0800

    Update NEU-AutoSign.py

--- NEU-AutoSign.py
+++ NEU-AutoSign.py
@@ -11,11 +11,9 @@ login_payload = {'username': ********, # 在这里填写用户名
                 'imageCodeResult':'' ,
                 'imageKey':'' }
 post_data = { } # 在这里填写个人数据
-__headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36',
-        'Content-Type': 'application/json;charset=UTF-8',
-        'Authorization': 'Basic dnVlOnZ1ZQ=='
-            }
+__headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36',
+             'Content-Type': 'application/json;charset=UTF-8',
+             'Authorization': 'Basic dnVlOnZ1ZQ=='}
 
 login_page = requests.post("http://stuinfo.neu.edu.cn/api/auth/oauth/token",
                             headers = __headers,

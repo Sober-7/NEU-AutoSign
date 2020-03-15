3ba198568944a0b57ed887e2f6fcf7c5c889cb56
Sober-7 <62213453+Sober-7@users.noreply.github.com>
2020-03-16 01:03:01 +0800

    Update NEU-AutoSign.py

--- NEU-AutoSign.py
+++ NEU-AutoSign.py
@@ -5,12 +5,76 @@ import re
 import time
 import json
 
-login_payload = {'username': ********, # 在这里填写用户名
+login_payload = {'username': 20194974, # 在这里填写用户名
                 'grant_type': 'password',
-                'password': ******, # 在这里填写密码
+                'password': 201094, # 在这里填写密码
                 'imageCodeResult':'' ,
                 'imageKey':'' }
-post_data = {} # 在这里填写个人数据
+post_data = {xm: "韩少波", xh: "20194974", xy: "计算机科学与工程学院", njjzy: "2019级电子信息类", bj: "电信类1901", xq: "浑南校区", ss: "三舍",…}
+xm: "韩少波"
+xh: "20194974"
+xy: "计算机科学与工程学院"
+njjzy: "2019级电子信息类"
+bj: "电信类1901"
+xq: "浑南校区"
+ss: "三舍"
+qy: "A"
+fjh: "446"
+brsjhm: "15737205029"
+sylx: "内地学生"
+jtxxdz_sf: "河南"
+jtxxdz_cs: "安阳"
+jtxxdz_qx: "林州市"
+jtxxdz: "茶店镇贝村西贝村7号"
+mqxxdz_sf: "河南"
+mqxxdz_cs: "安阳"
+mqxxdz_qx: "林州市"
+mqxxdz: "茶店镇贝村西贝村7号"
+mqjzdzsm: "学习"
+jzsjhm: "13703465441"
+mqzk: "A"
+zjtw: ""
+zzkssj: ""
+sfjy: ""
+sfyqjc: ""
+mqsfzj: ""
+jtms: ""
+glyyms: ""
+gldxxdz_sf: ""
+gldxxdz: ""
+mqstzk: ""
+sfgcyiqz: "否"
+cjlqk: "曾经医学观察，后隔离解除"
+dsjtqkms: ""
+hjnznl: "河南"
+qgnl: "无"
+sfqtdqlxs: "否"
+sfqtdqlxsmsxj: ""
+sfjcgbr: "否"
+sfjcgbrmsxj: ""
+sfjcglxsry: "否"
+sfjcglxsrymsxj: ""
+sfjcgysqzbr: "否"
+sfjcgysqzbrmsxj: ""
+sfjtcyjjfbqk: "否"
+sfjtcyjjfbqkmsxj: ""
+sfqgfrmz: "否"
+yljgmc: ""
+zzzd: ""
+sfygfr: "无"
+zgtw: ""
+zgtwcxsj: ""
+sfyghxdbsy: "无"
+sfyghxdbsycxsj: ""
+sfygxhdbsy: "无"
+sfygxhdbsycxsj: ""
+sfbrtb: "是"
+fdysfty: "否"
+tbrxm: ""
+tbrxh: ""
+tbrxy: ""
+dtyy: ""
+id: ""} # 在这里填写个人数据
 __headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36',
              'Content-Type': 'application/json;charset=UTF-8',
              'Authorization': 'Basic dnVlOnZ1ZQ=='}
